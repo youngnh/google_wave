c9cb177573dafabb43d570f0addae1e8f466d7c0
soafa-user <n@spectre.(none)>
2009-08-27 00:17:45 -0500

    getting the bot's profile picture and name setup

--- woot_bot/src/stl/hackfest/WootBot.java
+++ woot_bot/src/stl/hackfest/WootBot.java
@@ -2,20 +2,27 @@ package stl.hackfest;
 
 import com.google.wave.api.*;
 
+import java.util.logging.Logger;
+
 public class WootBot extends AbstractRobotServlet {
+
+    private static final Logger log = Logger.getLogger(WootBot.class.getName());
 
     @Override
     public void processEvents(RobotMessageBundle bundle) {
 	Wavelet wavelet = bundle.getWavelet();
 
 	if(bundle.wasSelfAdded()) {
+	    log.info("self was added");
 	    Blip blip = wavelet.appendBlip();
 	    TextView textView = blip.getDocument();
-	    textView.append("I'm alive!");
+	    textView.append("Woot Bot!");
 	}
 
 	for(Event e : bundle.getEvents()) {
+	    log.info("event type: " + e.getType().toString());
 	    if(e.getType() == EventType.WAVELET_PARTICIPANTS_CHANGED) {
+		log.info("wavelet participants changed");
 		Blip blip = wavelet.appendBlip();
 		TextView textView = blip.getDocument();
 		textView.append("Hi, everybody!");
